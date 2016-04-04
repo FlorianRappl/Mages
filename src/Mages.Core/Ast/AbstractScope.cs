50dffa39ce814ac485133aac59920928561a0a81
Florian Rappl <rappl.florian@gmail.com>
2016-04-04 22:49:09 +0200

    Added dictionary for book keeping

--- src/Mages.Core/Ast/AbstractScope.cs
+++ src/Mages.Core/Ast/AbstractScope.cs
@@ -1,10 +1,15 @@
 ﻿namespace Mages.Core.Ast
 {
-    class AbstractScope
+    using Mages.Core.Ast.Expressions;
+    using System;
+    using System.Collections.Generic;
+
+    sealed class AbstractScope
     {
         #region Fields
 
         private readonly AbstractScope _parent;
+        private readonly Dictionary<String, List<VariableExpression>> _references;
 
         #endregion
 
@@ -13,6 +18,7 @@
         public AbstractScope(AbstractScope parent)
         {
             _parent = parent;
+            _references = new Dictionary<String, List<VariableExpression>>();
         }
 
         #endregion
