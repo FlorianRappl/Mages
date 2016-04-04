383124e97b3afd68fdf0ae3366bb93f827de465b
Florian Rappl <rappl.florian@gmail.com>
2016-04-04 22:49:24 +0200

    Defined the tokenizer

--- src/Mages.Core/Tokens/ITokenizer.cs
+++ src/Mages.Core/Tokens/ITokenizer.cs
@@ -1,5 +1,7 @@
 ﻿namespace Mages.Core.Tokens
 {
+    using Mages.Core.Source;
+
     /// <summary>
     /// Represents the tokenizer performing the lexical analysis.
     /// </summary>
@@ -9,17 +11,6 @@
         /// Gets the next token from the scanner.
         /// </summary>
         /// <returns>The token.</returns>
-        IToken Next();
-
-        /// <summary>
-        /// Gets the previous token from the scanner
-        /// </summary>
-        /// <returns>The token.</returns>
-        IToken Previous();
-
-        /// <summary>
-        /// Gets the current token.
-        /// </summary>
-        IToken Current { get; }
+        IToken Next(IScanner scanner);
     }
 }
