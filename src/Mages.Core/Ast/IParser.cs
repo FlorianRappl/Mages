eb8fd20a2cff036d717a2aec09d67021872f1437
Florian Rappl <rappl.florian@gmail.com>
2016-04-06 23:55:38 +0200

    Provided interface partial definition

--- src/Mages.Core/Ast/IParser.cs
+++ src/Mages.Core/Ast/IParser.cs
@@ -1,11 +1,29 @@
 ﻿namespace Mages.Core.Ast
 {
+    using Mages.Core.Tokens;
     using System;
+    using System.Collections.Generic;
 
+    /// <summary>
+    /// Represents the core parser interface.
+    /// </summary>
     public interface IParser
     {
+        /// <summary>
+        /// Gets if any errors have been encountered.
+        /// </summary>
         Boolean HasErrors { get; }
 
+        /// <summary>
+        /// Gets the current error count.
+        /// </summary>
         Int32 ErrorCount { get; }
+
+        /// <summary>
+        /// Parses the next expression.
+        /// </summary>
+        /// <param name="tokens">The stream of tokens.</param>
+        /// <returns>The parsed expression.</returns>
+        IExpression ParseExpression(IEnumerator<IToken> tokens);
     }
 }
