25171024145034f0a9e13fa1776632435a5c4bb9
Florian Rappl <rappl.florian@gmail.com>
2016-04-09 17:14:09 +0200

    Added perf. benchmarking library

---
+++ src/Mages.Core.Performance/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Mages.Core.Performance")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Polytrope")]
+[assembly: AssemblyProduct("Mages")]
+[assembly: AssemblyCopyright("Copyright © 2016, Florian Rappl")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("dcfd7765-c8b5-4afc-aede-a4588c1d450f")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ src/Mages.Core.Performance/TrivialBenchmarks.cs
@@ -0,0 +1,14 @@
+﻿namespace Mages.Core.Performance
+{
+    using BenchmarkDotNet.Attributes;
+    using System;
+
+    public class TrivialBenchmarks
+    {
+        [Benchmark]
+        public Double Foo()
+        {
+            return Math.Exp(1.2);
+        }
+    }
+}
