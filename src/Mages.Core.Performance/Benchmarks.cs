b8c10aa0afc09d7112e1d1db90b3fb49091acd9b
Florian Rappl <rappl.florian@gmail.com>
2016-05-03 06:52:31 +0200

    Run first perf. benchmarks

---
+++ src/Mages.Core.Performance/Benchmarks.cs
@@ -0,0 +1,12 @@
+﻿namespace Mages.Core.Performance
+{
+    using BenchmarkDotNet.Running;
+
+    public static class Benchmarks
+    {
+        public static void Run()
+        {
+            BenchmarkRunner.Run<TrivialBenchmarks>();
+        }
+    }
+}
--- src/Mages.Core.Performance/TrivialBenchmarks.cs
+++ src/Mages.Core.Performance/TrivialBenchmarks.cs
@@ -2,31 +2,50 @@
 {
     using BenchmarkDotNet.Attributes;
     using System;
+    using YAMP;
 
     public class TrivialBenchmarks
     {
+        private static readonly String Test1 = "2 + 3";
+        private static readonly String Test2 = "2 + 17 + 25 * 4^(2 * 3) / 12";
+
         [Benchmark]
         public Double AddTwoNumbersWithYamp()
         {
-            return YampNumeric("2 + 3");
+            return YampNumeric(Test1);
         }
 
         [Benchmark]
         public Double AddTwoNumbersWithMages()
         {
-            return MagesNumeric("2 + 3");
+            return MagesNumeric(Test1);
         }
+
+        [Benchmark]
+        public Double AddMultiplyDivideAndPowerNumbersWithYamp()
+        {
+            return YampNumeric(Test2);
+        }
+
+        [Benchmark]
+        public Double AddMultiplyDivideAndPowerNumbersWithMages()
+        {
+            return MagesNumeric(Test2);
+        }
+
+        private static readonly Parser YampParser = new Parser();
+        private static readonly Engine MagesEngine = new Engine();
 
         private Double YampNumeric(String sourceCode)
         {
-            //TODO
-            return 10.0;
+            var result = YampParser.Evaluate(sourceCode);
+            return (result as ScalarValue).Value;
         }
 
         private Double MagesNumeric(String sourceCode)
         {
-            //TODO
-            return 10.0;
+            var result = MagesEngine.Interpret(sourceCode);
+            return (Double)result;
         }
     }
 }
