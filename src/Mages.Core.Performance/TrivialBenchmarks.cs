5c2737dc3c802eabd5e7e93f9c409daa647a85d0
Florian Rappl <rappl.florian@gmail.com>
2016-05-23 18:30:38 +0200

    Added more benchmarks #4

--- src/Mages.Core.Performance/TrivialBenchmarks.cs
+++ src/Mages.Core.Performance/TrivialBenchmarks.cs
@@ -6,9 +6,11 @@
 
     public class TrivialBenchmarks
     {
-        private static readonly String Test1 = "2 + 3";
-        private static readonly String Test2 = "2 + 17 + 25 * 4^(2 * 3) / 12";
-        private static readonly String Test3 = "a = 3; b = 6; a * b";
+        private static readonly String AddTwoNumbers = "2 + 3";
+        private static readonly String AddMultiplyDivideAndPowerNumbers = "2 + 17 + 25 * 4^(2 * 3) / 12";
+        private static readonly String MultiplyTwoVariables = "a = 3; b = 6; a * b";
+        private static readonly String CallStandardFunctions = "sin(pi / 4) * cos(pi * 0.25) + exp(2) * log(3)";
+        private static readonly String Transpose4x5Matrix = "M = [1,2,3,4,5;6,7,8,9,10;11,12,13,14,15;16,17+2,18/3,19*4,20-5]'; 0";
 
         private static readonly Parser YampParser = new Parser();
         private static readonly Engine MagesEngine = new Engine();
@@ -16,43 +18,67 @@
         [Benchmark]
         public Double Yamp_AddTwoNumbers()
         {
-            return YampNumeric(Test1);
+            return YampNumeric(AddTwoNumbers);
         }
 
         [Benchmark]
         public Double Mages_AddTwoNumbers()
         {
-            return MagesNumeric(Test1);
+            return MagesNumeric(AddTwoNumbers);
         }
 
         [Benchmark]
         public Double Yamp_AddMultiplyDivideAndPowerNumbers()
         {
-            return YampNumeric(Test2);
+            return YampNumeric(AddMultiplyDivideAndPowerNumbers);
         }
 
         [Benchmark]
         public Double Mages_AddMultiplyDivideAndPowerNumbers()
         {
-            return MagesNumeric(Test2);
+            return MagesNumeric(AddMultiplyDivideAndPowerNumbers);
         }
 
         [Benchmark]
         public Double Yamp_MultiplyTwoVariables()
         {
-            return YampNumeric(Test3);
+            return YampNumeric(MultiplyTwoVariables);
         }
 
         [Benchmark]
         public Double Mages_MultiplyTwoVariables()
         {
-            return MagesNumeric(Test3);
+            return MagesNumeric(MultiplyTwoVariables);
+        }
+
+        [Benchmark]
+        public Double Yamp_CallStandardFunctions()
+        {
+            return YampNumeric(CallStandardFunctions);
+        }
+
+        [Benchmark]
+        public Double Mages_CallStandardFunctions()
+        {
+            return MagesNumeric(CallStandardFunctions);
+        }
+
+        [Benchmark]
+        public Double Yamp_Transpose4x5Matrix()
+        {
+            return YampNumeric(Transpose4x5Matrix);
+        }
+
+        [Benchmark]
+        public Double Mages_Transpose4x5Matrix()
+        {
+            return MagesNumeric(Transpose4x5Matrix);
         }
 
         private Double YampNumeric(String sourceCode)
         {
             var result = YampParser.Evaluate(sourceCode);
-            return (result as ScalarValue).Value;
+            return ((ScalarValue)result).Value;
         }
 
         private Double MagesNumeric(String sourceCode)
