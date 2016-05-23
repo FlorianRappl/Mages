a48e27980aca942d4557f66b2d2850579348f7bc
Florian Rappl <rappl.florian@gmail.com>
2016-05-23 09:32:18 +0200

    Added comments

--- src/Mages.Core/ErrorCode.cs
+++ src/Mages.Core/ErrorCode.cs
@@ -7,66 +7,159 @@
     /// </summary>
     public enum ErrorCode
     {
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("A terminator has been expected ( ';' ).")]
         TerminatorExpected,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("The input does not represent a valid identifier.")]
         IdentifierExpected,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("The expression term is invalid.")]
         InvalidExpression,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("A block of statements needs to be properly terminated ( '}' ).")]
         BlockNotTerminated,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("The given symbol cannot be used.")]
         InvalidSymbol,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("The scientific notation is missing the exponent.")]
         ScientificMismatch,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("The floating point number is truncated.")]
         FloatingMismatch,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("A block comment needs to be properly terminated ( '*/' ).")]
         BlockCommentNotTerminated,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("The string literal is missing the terminator ( '\"' ).")]
         StringMismatch,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("The given escape sequence is invalid.")]
         EscapeSequenceInvalid,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("The ASCII character escape sequence is invalid.")]
         AsciiSequenceInvalid,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("The unicode character escape sequence is invalid.")]
         UnicodeSequenceInvalid,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("A unary operator requires at least one operand.")]
         OperandRequired,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("The increment operand has to be a valid identifier.")]
         IncrementOperand,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("The decrement operand has to be a valid identifier.")]
         DecrementOperand,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("The binary operator is missing a left operand.")]
         LeftOperandRequired,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("The binary operator is missing a right operand.")]
         RightOperandRequired,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("A range requires a valid start value.")]
         RangeStartRequired,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("The step of a range has to be valid.")]
         RangeStepError,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("A range must have a valid end value.")]
         RangeEndRequired,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("The given member is invalid. Only valid identifiers represent members.")]
         MemberInvalid,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("The matrix needs to be properly terminated ( ']' ).")]
         MatrixNotTerminated,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("The object needs to be properly terminated ( '}' ).")]
         ObjectNotTerminated,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("An expected closing bracket could not be found ( ']' ).")]
         IndicesNotTerminated,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("An expected closing paranthesis could not be found ( ')' ).")]
         BracketNotTerminated,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("An expected opening bracket could not be found ( '{' ).")]
         BracketExpected,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("An assignable expression has been expected.")]
         AssignableExpected,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("Too many indices provided. A maximum of 2 indices is allowed.")]
         TooManyIndices,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("The conditional operator is missing the alternative branch.")]
         BranchMissing,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("The given keyword has been misplaced.")]
         KeywordUnexpected,
+        /// <summary>
+        /// See description.
+        /// </summary>
         [Description("An expected colon has not been found.")]
         ColonExpected,
     }
--- src/Mages.Core/Tokens/TokenType.cs
+++ src/Mages.Core/Tokens/TokenType.cs
@@ -5,46 +5,169 @@
     /// </summary>
     public enum TokenType
     {
+        /// <summary>
+        /// Unknown token type.
+        /// </summary>
         Unknown = 0,
+        /// <summary>
+        /// Keyword token.
+        /// </summary>
         Keyword = 1,
+        /// <summary>
+        /// Identifier token.
+        /// </summary>
         Identifier = 2,
+        /// <summary>
+        /// Number token.
+        /// </summary>
         Number = 3,
+        /// <summary>
+        /// String token.
+        /// </summary>
         Text = 5,
+        /// <summary>
+        /// Round bracket open.
+        /// </summary>
         OpenGroup = 6,
+        /// <summary>
+        /// Round bracket close.
+        /// </summary>
         CloseGroup = 7,
+        /// <summary>
+        /// Square bracket open.
+        /// </summary>
         OpenList = 8,
+        /// <summary>
+        /// Square bracket close.
+        /// </summary>
         CloseList = 9,
+        /// <summary>
+        /// Curly bracket open.
+        /// </summary>
         OpenScope = 10,
+        /// <summary>
+        /// Curly bracket close.
+        /// </summary>
         CloseScope = 11,
+        /// <summary>
+        /// Comma symbol.
+        /// </summary>
         Comma = 12,
+        /// <summary>
+        /// Colon symbol.
+        /// </summary>
         Colon = 13,
+        /// <summary>
+        /// Dot symbol.
+        /// </summary>
         Dot = 14,
+        /// <summary>
+        /// Power operator.
+        /// </summary>
         Power = 15,
+        /// <summary>
+        /// Right divide operator.
+        /// </summary>
         RightDivide = 16,
+        /// <summary>
+        /// Left divide operator.
+        /// </summary>
         LeftDivide = 17,
+        /// <summary>
+        /// Multiply operator.
+        /// </summary>
         Multiply = 18,
+        /// <summary>
+        /// Modulo operator.
+        /// </summary>
         Modulo = 19,
+        /// <summary>
+        /// Factorial operator.
+        /// </summary>
         Factorial = 20,
+        /// <summary>
+        /// Add operator.
+        /// </summary>
         Add = 21,
+        /// <summary>
+        /// Subtract operator.
+        /// </summary>
         Subtract = 22,
+        /// <summary>
+        /// Increment operator.
+        /// </summary>
         Increment = 23,
+        /// <summary>
+        /// Decrement operator.
+        /// </summary>
         Decrement = 24,
+        /// <summary>
+        /// Or operator.
+        /// </summary>
         Or = 25,
+        /// <summary>
+        /// And operator.
+        /// </summary>
         And = 26,
+        /// <summary>
+        /// Equal operator.
+        /// </summary>
         Equal = 27,
+        /// <summary>
+        /// Not equal operator.
+        /// </summary>
         NotEqual = 28,
+        /// <summary>
+        /// Less operator.
+        /// </summary>
         Less = 29,
+        /// <summary>
+        /// Less or equal operator.
+        /// </summary>
         LessEqual = 30,
+        /// <summary>
+        /// Greater or equal operator.
+        /// </summary>
         GreaterEqual = 31,
+        /// <summary>
+        /// Greater operator.
+        /// </summary>
         Greater = 32,
+        /// <summary>
+        /// Negate operator.
+        /// </summary>
         Negate = 33,
+        /// <summary>
+        /// Condition operator.
+        /// </summary>
         Condition = 34,
+        /// <summary>
+        /// Transpose operator.
+        /// </summary>
         Transpose = 35,
+        /// <summary>
+        /// Any kind of space symbol.
+        /// </summary>
         Space = 36,
+        /// <summary>
+        /// Assignment operator.
+        /// </summary>
         Assignment = 37,
+        /// <summary>
+        /// Lambda operator.
+        /// </summary>
         Lambda = 38,
+        /// <summary>
+        /// Semicolon symbol.
+        /// </summary>
         SemiColon = 0xff,
+        /// <summary>
+        /// Comment token.
+        /// </summary>
         Comment = 0xfffe,
+        /// <summary>
+        /// EOF.
+        /// </summary>
         End = 0xffff,
     }
 }
