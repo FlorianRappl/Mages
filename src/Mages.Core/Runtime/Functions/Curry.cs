3f1f80477e6b84e253f2452e14f4beed9a7dfbe8
Florian Rappl <rappl.florian@gmail.com>
2016-05-31 13:19:49 +0200

    Helpers for writing .NET MAGES functions

--- src/Mages.Core/Runtime/Functions/Curry.cs
+++ src/Mages.Core/Runtime/Functions/Curry.cs
@@ -2,13 +2,30 @@
 {
     using System;
 
-    static class Curry
+    /// <summary>
+    /// Provide helpers to enable currying.
+    /// </summary>
+    public static class Curry
     {
+        /// <summary>
+        /// Checks if the provided args deliver at least one argument.
+        /// Otherwise returns null.
+        /// </summary>
+        /// <param name="function">The function to return or capture.</param>
+        /// <param name="args">The args to check and potentially capture.</param>
+        /// <returns>A curried function or null.</returns>
         public static Object MinOne(Function function, Object[] args)
         {
             return args.Length < 1 ? function : null;
         }
 
+        /// <summary>
+        /// Checks if the provided args deliver at least two arguments.
+        /// Otherwise returns null.
+        /// </summary>
+        /// <param name="function">The function to return or capture.</param>
+        /// <param name="args">The args to check and potentially capture.</param>
+        /// <returns>A curried function or null.</returns>
         public static Object MinTwo(Function function, Object[] args)
         {
             if (args.Length < 2)
@@ -19,6 +36,14 @@
             return null;
         }
 
+        /// <summary>
+        /// Checks if the provided args deliver at least count argument(s).
+        /// Otherwise returns null.
+        /// </summary>
+        /// <param name="count">The required number of arguments.</param>
+        /// <param name="function">The function to return or capture.</param>
+        /// <param name="args">The args to check and potentially capture.</param>
+        /// <returns>A curried function or null.</returns>
         public static Object Min(Int32 count, Function function, Object[] args)
         {
             if (args.Length < count)
--- src/Mages.Core/Runtime/Functions/If.cs
+++ src/Mages.Core/Runtime/Functions/If.cs
@@ -2,16 +2,85 @@
 {
     using System;
 
-    static class If
+    /// <summary>
+    /// Helpers to check for argument types.
+    /// </summary>
+    public static class If
     {
+        /// <summary>
+        /// Checks if the first value of the given arguments is of type T.
+        /// </summary>
+        /// <typeparam name="T">The type T to check for.</typeparam>
+        /// <param name="args">The arguments to check.</param>
+        /// <param name="f">The callback to invoke if fulfilled.</param>
+        /// <returns>The result of the callback or null.</returns>
         public static Object Is<T>(Object[] args, Func<T, Object> f)
         {
             return args[0] is T ? f((T)args[0]) : null;
         }
 
-        public static Object Is<Tx, Ty>(Object[] args, Func<Tx, Ty, Object> f)
+        /// <summary>
+        /// Checks if the first two values of the given arguments are of type
+        /// T1 and T2.
+        /// </summary>
+        /// <typeparam name="T1">The type of the first value.</typeparam>
+        /// <typeparam name="T2">The type of the second value.</typeparam>
+        /// <param name="args">The arguments to check.</param>
+        /// <param name="f">The callback to invoke if fulfilled.</param>
+        /// <returns>The result of the callback or null.</returns>
+        public static Object Is<T1, T2>(Object[] args, Func<T1, T2, Object> f)
         {
-            return args[0] is Tx && args[1] is Ty ? f((Tx)args[0], (Ty)args[1]) : null;
+            return args[0] is T1 && args[1] is T2 ? f((T1)args[0], (T2)args[1]) : null;
+        }
+
+        /// <summary>
+        /// Checks if the first three values of the given arguments are of type
+        /// T1, T2, and T3.
+        /// </summary>
+        /// <typeparam name="T1">The type of the first value.</typeparam>
+        /// <typeparam name="T2">The type of the second value.</typeparam>
+        /// <typeparam name="T3">The type of the third value.</typeparam>
+        /// <param name="args">The arguments to check.</param>
+        /// <param name="f">The callback to invoke if fulfilled.</param>
+        /// <returns>The result of the callback or null.</returns>
+        public static Object Is<T1, T2, T3>(Object[] args, Func<T1, T2, T3, Object> f)
+        {
+            return args[0] is T1 && args[1] is T2 && args[2] is T3 ? f((T1)args[0], (T2)args[1], (T3)args[2]) : null;
+        }
+
+        /// <summary>
+        /// Checks if the first four values of the given arguments are of type
+        /// T1, T2, T3, and T4.
+        /// </summary>
+        /// <typeparam name="T1">The type of the first value.</typeparam>
+        /// <typeparam name="T2">The type of the second value.</typeparam>
+        /// <typeparam name="T3">The type of the third value.</typeparam>
+        /// <typeparam name="T4">The type of the fourth value.</typeparam>
+        /// <param name="args">The arguments to check.</param>
+        /// <param name="f">The callback to invoke if fulfilled.</param>
+        /// <returns>The result of the callback or null.</returns>
+        public static Object Is<T1, T2, T3, T4>(Object[] args, Func<T1, T2, T3, T4, Object> f)
+        {
+            return args[0] is T1 && args[1] is T2 && args[2] is T3 && args[3] is T4 ? 
+                f((T1)args[0], (T2)args[1], (T3)args[2], (T4)args[3]) : null;
+        }
+
+        /// <summary>
+        /// Checks if the first five values of the given arguments are of type
+        /// T1, T2, T3, T4 and T5.
+        /// </summary>
+        /// <typeparam name="T1">The type of the first value.</typeparam>
+        /// <typeparam name="T2">The type of the second value.</typeparam>
+        /// <typeparam name="T3">The type of the third value.</typeparam>
+        /// <typeparam name="T4">The type of the fourth value.</typeparam>
+        /// <typeparam name="T5">The type of the fifth value.</typeparam>
+        /// <param name="args">The arguments to check.</param>
+        /// <param name="f">The callback to invoke if fulfilled.</param>
+        /// <returns>The result of the callback or null.</returns>
+        public static Object Is<T1, T2, T3, T4, T5>(Object[] args, Func<T1, T2, T3, T4, T5, Object> f)
+        {
+            return args[0] is T1 && args[1] is T2 && args[2] is T3 && args[3] is T4 && args[4] is T5 ?
+                f((T1)args[0], (T2)args[1], (T3)args[2], (T4)args[3], (T5)args[4]) : null;
         }
     }
 }
