4d36f59073a0418d0007eefabf8c3600d4b2d93a
Florian Rappl <rappl.florian@gmail.com>
2016-06-03 15:56:36 +0200

    Improved string builder usage for JSON serialization

--- src/Mages.Core/Runtime/JsonSerializer.cs
+++ src/Mages.Core/Runtime/JsonSerializer.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Text;
 
     sealed class JsonSerializer
     {
@@ -14,70 +15,76 @@
 
         public String Serialize(Object value)
         {
+            var sb = StringBuilderPool.Pull();
+            SerializeTo(value, sb, 0);
+            return sb.Stringify();
+        }
+
+        private void SerializeTo(IDictionary<String, Object> obj, StringBuilder buffer, Int32 level)
+        {
+            var index = 0;
+            _seen.Add(obj);
+            buffer.AppendLine("{");
+
+            foreach (var item in obj)
+            {
+                var sublevel = level + 1;
+                var key = Stringify.AsJson(item.Key);
+                buffer.Append(' ', 2 * sublevel).Append(key).Append(": ");
+                SerializeTo(item.Value, buffer, sublevel);
+
+                if (index + 1 < obj.Count)
+                {
+                    buffer.Append(',');
+                }
+
+                buffer.AppendLine();
+            }
+
+            buffer.Append(' ', 2 * level).Append('}');
+        }
+
+        private void SerializeTo(Object value, StringBuilder buffer, Int32 level)
+        {
             if (value == null)
             {
-                return "null";
+                buffer.Append("null");
             }
             else if (value is Function)
             {
-                return Stringify.AsJson("[Function]");
+                buffer.Append(Stringify.AsJson("[Function]"));
             }
             else if (value is IDictionary<String, Object>)
             {
                 if (!_seen.Contains(value))
                 {
-                    _seen.Add(value);
-                    return AsJson((IDictionary<String, Object>)value);
+                    SerializeTo((IDictionary<String, Object>)value, buffer, level);
                 }
-
-                return Stringify.AsJson("[Recursion]");
+                else
+                {
+                    buffer.Append(Stringify.AsJson("[Recursion]"));
+                }
             }
             else if (value is Double[,])
             {
-                return Stringify.AsJson((Double[,])value);
+                buffer.Append(Stringify.AsJson((Double[,])value));
             }
             else if (value is String)
             {
-                return Stringify.AsJson((String)value);
+                buffer.Append(Stringify.AsJson((String)value));
             }
             else if (value is Double)
             {
-                return Stringify.This((Double)value);
+                buffer.Append(Stringify.This((Double)value));
             }
             else if (value is Boolean)
             {
-                return Stringify.This((Boolean)value);
+                buffer.Append(Stringify.This((Boolean)value));
             }
-
-            return "undefined";
-        }
-
-        /// <summary>
-        /// Converts the given dictionary object to a JSON string.
-        /// </summary>
-        /// <param name="obj">The object to represent.</param>
-        /// <returns>The JSON representation.</returns>
-        public String AsJson(IDictionary<String, Object> obj)
-        {
-            var sb = StringBuilderPool.Pull();
-            sb.AppendLine("{");
-
-            foreach (var item in obj)
+            else
             {
-                var key = Stringify.AsJson(item.Key);
-                var value = Serialize(item.Value);
-
-                if (value.Contains(Environment.NewLine))
-                {
-                    var lines = value.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
-                    value = String.Join(Environment.NewLine + "  ", lines);
-                }
-
-                sb.Append("  ").Append(key).Append(": ").AppendLine(value);
+                buffer.Append("undefined");
             }
-
-            sb.Append('}');
-            return sb.Stringify();
         }
     }
 }
