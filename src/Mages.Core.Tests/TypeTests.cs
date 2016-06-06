50ab58b3b91708f946f5bf3ef33a369533aea5b8
Florian Rappl <rappl.florian@gmail.com>
2016-06-06 16:38:17 +0200

    Added more tests to verify #29

--- src/Mages.Core.Tests/TypeTests.cs
+++ src/Mages.Core.Tests/TypeTests.cs
@@ -34,6 +34,55 @@
             Assert.AreEqual(1.0, result);
         }
 
+        [Test]
+        public void IsFunctionStringIsString()
+        {
+            var result = Eval("is(\"String\", \"Test string\") // true");
+            Assert.AreEqual(true, result);
+        }
+                
+        [Test]
+        public void IsFunctionBooleanAintNumber()
+        {
+            var result = Eval("is(\"Number\", true) // false");
+            Assert.AreEqual(false, result);
+        }
+
+        [Test]
+        public void IsFunctionBooleanExpressionIsCurriedBoolean()
+        {
+            var result = Eval("is_bool = is(\"Boolean\"); is_bool(14 ~= 7) // true");
+            Assert.AreEqual(true, result);
+        }
+
+        [Test]
+        public void AsFunctionConvertsNumberToString()
+        {
+            var result = Eval("as(\"String\", 3.5) // \"3.5\"");
+            Assert.AreEqual("3.5", result);
+        }
+
+        [Test]
+        public void AsFunctionConvertsBooleanToMatrix()
+        {
+            var result = Eval("as(\"Matrix\", true) // [1]");
+            CollectionAssert.AreEquivalent(new Double[,] { { 1.0 } }, (Double[,])result);
+        }
+
+        [Test]
+        public void AsFunctionConvertsNullToString()
+        {
+            var result = Eval("as(\"String\", null)");
+            Assert.AreEqual(String.Empty, result);
+        }
+
+        [Test]
+        public void AsFunctionConvertsStringToNull()
+        {
+            var result = Eval("as(\"Undefined\", \"Hi\")");
+            Assert.AreEqual(null, result);
+        }
+
         private static Object Eval(String source)
         {
             var engine = new Engine();
