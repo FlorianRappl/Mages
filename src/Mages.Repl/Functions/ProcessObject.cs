fedf23d6d145718523cdb6e652e29ce449837ae4
Florian Rappl <rappl.florian@gmail.com>
2016-06-07 08:34:04 +0200

    Extended the process with environment variables

--- src/Mages.Repl/Functions/ProcessObject.cs
+++ src/Mages.Repl/Functions/ProcessObject.cs
@@ -1,20 +1,35 @@
 ﻿namespace Mages.Repl.Functions
 {
     using System;
+    using System.Collections;
+    using System.Collections.Generic;
     using System.Diagnostics;
+    using System.Linq;
 
     sealed class ProcessObject
     {
         private readonly Process _process;
+        private readonly IDictionary<String, Object> _env;
 
         public ProcessObject()
         {
             _process = Process.GetCurrentProcess();
+            _env = Environment.GetEnvironmentVariables().OfType<DictionaryEntry>().ToDictionary(m => (String)m.Key, m => m.Value);
         }
 
         public Int32 id
         {
             get { return _process.Id; }
+        }
+
+        public String workingDirectory
+        {
+            get { return Environment.CurrentDirectory; }
+        }
+
+        public Int32 processors
+        {
+            get { return Environment.ProcessorCount; }
         }
 
         public String machine
@@ -40,6 +55,11 @@
         public Int32 threads
         {
             get { return _process.Threads.Count; }
+        }
+
+        public IDictionary<String, Object> env
+        {
+            get { return _env; }
         }
     }
 }
