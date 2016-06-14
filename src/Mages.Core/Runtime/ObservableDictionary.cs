0de479d792a25d68bcd7686d262adb2783d8a24a
Florian Rappl <rappl.florian@gmail.com>
2016-06-14 15:00:13 +0200

    Extended with XML doc. #30

--- src/Mages.Core/Runtime/ObservableDictionary.cs
+++ src/Mages.Core/Runtime/ObservableDictionary.cs
@@ -5,102 +5,157 @@
     using System.Collections.Generic;
     using System.Linq;
 
+    /// <summary>
+    /// Represents the observable dictionary from MAGES.
+    /// </summary>
     public class ObservableDictionary : IDictionary<String, Object>
     {
-        private readonly IDictionary<String, Object> _store;
+        #region Fields
 
+        private readonly IDictionary<String, Object> _container;
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Fired once an element is added, removed, or updated.
+        /// </summary>
         public event EventHandler<EntryChangedArgs> Changed;
 
-        public ObservableDictionary(IDictionary<String, Object> store)
+        #endregion
+
+        #region ctor
+
+        /// <summary>
+        /// Creates a new observable dictionary with a standard container.
+        /// </summary>
+        public ObservableDictionary()
+            : this(new Dictionary<String, Object>())
         {
-            _store = store;
         }
 
-        Object IDictionary<String, Object>.this[String key]
+        /// <summary>
+        /// Creates a new observable dictionary with the given container.
+        /// </summary>
+        public ObservableDictionary(IDictionary<String, Object> container)
         {
-            get { return _store[key]; }
+            _container = container;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the used container.
+        /// </summary>
+        public IDictionary<String, Object> Container
+        {
+            get { return _container; }
+        }
+
+        #endregion
+
+        #region IDictionary Implementation
+
+        /// <summary>
+        /// Gets or sets the value at key.
+        /// </summary>
+        public Object this[String key]
+        {
+            get { return _container[key]; }
             set
             {
                 var existing = default(Object);
-                _store.TryGetValue(key, out existing);
-                _store[key] = value;
+                _container.TryGetValue(key, out existing);
+                _container[key] = value;
                 Emit(key, existing, value);
             }
         }
 
-        Int32 ICollection<KeyValuePair<String, Object>>.Count
+        /// <summary>
+        /// Gets the number of items stored in the container.
+        /// </summary>
+        public Int32 Count
         {
-            get { return _store.Count; }
+            get { return _container.Count; }
         }
 
         Boolean ICollection<KeyValuePair<String, Object>>.IsReadOnly
         {
-            get { return _store.IsReadOnly; }
+            get { return _container.IsReadOnly; }
         }
 
         ICollection<String> IDictionary<String, Object>.Keys
         {
-            get { return _store.Keys; }
+            get { return _container.Keys; }
         }
 
         ICollection<Object> IDictionary<String, Object>.Values
         {
-            get { return _store.Values; }
+            get { return _container.Values; }
         }
 
         void ICollection<KeyValuePair<String, Object>>.Add(KeyValuePair<String, Object> item)
         {
-            _store.Add(item);
+            _container.Add(item);
             Emit(item.Key, null, item.Value);
         }
 
-        void IDictionary<String, Object>.Add(String key, Object value)
+        /// <summary>
+        /// Adds the given key, value pair to the container.
+        /// </summary>
+        public void Add(String key, Object value)
         {
-            _store.Add(key, value);
+            _container.Add(key, value);
             Emit(key, null, value);
         }
 
-        void ICollection<KeyValuePair<String, Object>>.Clear()
+        /// <summary>
+        /// Resets the container.
+        /// </summary>
+        public void Clear()
         {
-            var items = _store.ToArray();
+            var items = _container.ToArray();
             
             foreach (var item in items)
             {
-                _store.Remove(item);
+                _container.Remove(item);
                 Emit(item.Key, item.Value, null);
             }
         }
 
         Boolean ICollection<KeyValuePair<String, Object>>.Contains(KeyValuePair<String, Object> item)
         {
-            return _store.Contains(item);
+            return _container.Contains(item);
         }
 
         Boolean IDictionary<String, Object>.ContainsKey(String key)
         {
-            return _store.ContainsKey(key);
+            return _container.ContainsKey(key);
         }
 
         void ICollection<KeyValuePair<String, Object>>.CopyTo(KeyValuePair<String, Object>[] array, Int32 arrayIndex)
         {
-            _store.CopyTo(array, arrayIndex);
+            _container.CopyTo(array, arrayIndex);
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return _store.GetEnumerator();
+            return _container.GetEnumerator();
         }
 
         IEnumerator<KeyValuePair<String, Object>> IEnumerable<KeyValuePair<String, Object>>.GetEnumerator()
         {
-            return _store.GetEnumerator();
+            return _container.GetEnumerator();
         }
 
         Boolean ICollection<KeyValuePair<String, Object>>.Remove(KeyValuePair<String, Object> item)
         {
-            if (_store.Contains(item))
+            if (_container.Contains(item))
             {
-                _store.Remove(item);
+                _container.Remove(item);
                 Emit(item.Key, item.Value, null);
                 return true;
             }
@@ -108,13 +163,16 @@
             return false;
         }
 
-        Boolean IDictionary<String, Object>.Remove(String key)
+        /// <summary>
+        /// Removes the given key from the container.
+        /// </summary>
+        public Boolean Remove(String key)
         {
             var existing = default(Object);
 
-            if (_store.TryGetValue(key, out existing))
+            if (_container.TryGetValue(key, out existing))
             {
-                _store.Remove(key);
+                _container.Remove(key);
                 Emit(key, existing, null);
                 return true;
             }
@@ -122,11 +180,24 @@
             return false;
         }
 
-        Boolean IDictionary<String, Object>.TryGetValue(String key, out Object value)
+        /// <summary>
+        /// Tries to get the value at the given key.
+        /// </summary>
+        public Boolean TryGetValue(String key, out Object value)
         {
-            return _store.TryGetValue(key, out value);
+            return _container.TryGetValue(key, out value);
         }
 
+        #endregion
+
+        #region Helpers
+
+        /// <summary>
+        /// Called after something changed.
+        /// </summary>
+        /// <param name="key">The name of the changed entry.</param>
+        /// <param name="oldValue">The entry's previous value.</param>
+        /// <param name="newValue">The entry's new value.</param>
         protected virtual void OnChanged(String key, Object oldValue, Object newValue)
         {
         }
@@ -142,5 +213,7 @@
 
             OnChanged(key, oldValue, newValue);
         }
+
+        #endregion
     }
 }
