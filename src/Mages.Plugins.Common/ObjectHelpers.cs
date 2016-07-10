46114d5bb699ab1d942f022ae0ccd570de22e25b
Florian Rappl <rappl.florian@gmail.com>
2016-07-10 16:55:32 +0200

    Added shared plugin library

---
+++ src/Mages.Plugins.Common/ObjectHelpers.cs
@@ -0,0 +1,20 @@
+﻿namespace Mages.Plugins
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class ObjectHelpers
+    {
+        public static IDictionary<String, Object> CreateObject(params Object[] keyValues)
+        {
+            var dict = new Dictionary<String, Object>();
+
+            for (var i = 0; i < keyValues.Length; i += 2)
+            {
+                dict[(String)keyValues[i]] = keyValues[i + 1];
+            }
+
+            return dict;
+        }
+    }
+}
