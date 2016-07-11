48c9435431a9ec9f9e28e4f9639b550bba2fdeea
Matteo Fabbri <matteo@phascode.org>
2016-07-11 19:02:17 +0200

    better implementation

--- src/Mages.Core/Runtime/Functions/IterationFunctions.cs
+++ src/Mages.Core/Runtime/Functions/IterationFunctions.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections;
 
 namespace Mages.Core.Runtime.Functions
 {
@@ -15,49 +11,34 @@ namespace Mages.Core.Runtime.Functions
         {
             if (args.Length == 0) return null;
 
-            if (args.Length == 1 && args[0] is Function)
+            var outer = args[0] as Function;
+
+            if (args.Length == 1 && outer!=null)
                 return new Function(arr =>
                 {
-                    var outer = args[0] as Function;
+                    var inner = arr[0] as IEnumerable;
 
-                    if (arr[0] is IEnumerable)
+                    foreach (var item in inner)
                     {
-                        var inner = arr[0] as IEnumerable;
-
-                        foreach (var item in inner)
-                        {
-                            outer.Invoke(new[] { item });
-                        }
+                        outer.Invoke(new[] { item });
                     }
-                    return outer.Invoke(new[] { args[0] });
+                    return arr[0];
                 });
 
             var f = args[1] as Function;
 
             if (args.Length == 2 && f!=null)
             {
-                if (args[0] is IEnumerable)
+                var enu = args[0] as IEnumerable;
+
+                if (enu!=null)
                     return new Function(arr =>
                     {
-                        var enu = args[0] as IEnumerable;
-
                         foreach (var item in enu)
                         {
                             f.Invoke(new[] { item });
                         }
-                        return null;
-                    });
-
-                if (args[0] is Function)
-                    return new Function(arr =>
-                    {
-                        var enu = (args[0] as Function).Invoke(new object[] { }) as IEnumerable;
-                       
-                        foreach (var item in enu)
-                        {
-                            f.Invoke(new[] { item });
-                        }
-                        return null;
+                        return args[0];
                     });
             }
 
