f18e25f2d691559128a6781b7a37c900daea715c
Matteo Fabbri <matteo@phascode.org>
2016-07-12 11:29:14 +0200

    Select & Where

--- src/Mages.Core/Runtime/Functions/IterationFunctions.cs
+++ src/Mages.Core/Runtime/Functions/IterationFunctions.cs
@@ -1,8 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Mages.Core.Runtime.Functions
 {
@@ -52,6 +51,118 @@ namespace Mages.Core.Runtime.Functions
                     f.Invoke(new[] { item });
                 }
                 return args[0];
+            }
+            return null;
+        });
+
+        /// <summary>
+        /// Linq style Where
+        /// </summary>
+        public static readonly Function Where = new Function(args =>
+        {
+            if (args.Length == 1)
+            {
+                if (args[0] is IEnumerable)
+                    return new Function(innerArg =>
+                    {
+                        var f = innerArg[0] as Function;
+                        var enu = args[0] as IEnumerable;
+
+                        var list = new List<object>();
+                        foreach (var item in enu)
+                        {
+                            if ((bool)f.Invoke(new[] { item }))
+                            {
+                                list.Add(item);
+                            }
+                        }
+                        return list;
+                    });
+
+                if (args[0] is Function)
+                    return new Function(innerArg =>
+                    {
+                        var f = args[0] as Function;
+                        var enu = innerArg[0] as IEnumerable;
+
+                        var list = new List<object>();
+                        foreach (var item in enu)
+                        {
+                            if ((bool)f.Invoke(new[] { item }))
+                            {
+                                list.Add(item);
+                            }
+                        }
+                        return list;
+                    });
+            }
+
+            if (args.Length == 2 && args[0] is IEnumerable && args[1] is Function)
+            {
+                var enu = args[0] as IEnumerable;
+                var f = args[1] as Function;
+
+                var list = new List<object>();
+                foreach (var item in enu)
+                {
+                    if ((bool)f.Invoke(new[] { item }))
+                    {
+                        list.Add(item);
+                    }
+                }
+                return list;
+            }
+            return null;
+        });
+
+
+        /// <summary>
+        /// Linq style Select
+        /// </summary>
+        public static readonly Function Select = new Function(args =>
+        {
+            if (args.Length == 1)
+            {
+                if (args[0] is IEnumerable)
+                    return new Function(innerArg =>
+                    {
+                        var f = innerArg[0] as Function;
+                        var enu = args[0] as IEnumerable;
+
+                        var list = new List<object>();
+                        foreach (var item in enu)
+                        {
+                            list.Add(f.Invoke(new[] { item }));
+                        }
+                        return list;
+                    });
+
+                if (args[0] is Function)
+                    return new Function(innerArg =>
+                    {
+                        var f = args[0] as Function;
+                        var enu = innerArg[0] as IEnumerable;
+
+                        var list = new List<object>();
+                        foreach (var item in enu)
+                        {
+                            list.Add(f.Invoke(new[] { item }));
+                        }
+                        return list;
+                    });
+            }
+
+            if (args.Length == 2 && args[0] is IEnumerable && args[1] is Function)
+            {
+                var enu = args[0] as IEnumerable;
+                var f = args[1] as Function;
+
+                var list = new List<object>();
+                foreach (var item in enu)
+                {
+                    list.Add(f.Invoke(new[] { item }));
+                }
+                return list;
             }
             return null;
         });
