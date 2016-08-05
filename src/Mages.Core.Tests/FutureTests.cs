94502a643f45be3029a09546d0703ba5c24d64ab
Florian Rappl <rappl.florian@gmail.com>
2016-08-05 13:27:41 +0200

    Test behvior in function

--- src/Mages.Core.Tests/FutureTests.cs
+++ src/Mages.Core.Tests/FutureTests.cs
@@ -21,6 +21,33 @@ sum
 ", 15.0, engine => engine.SetFunction("calcAsync", FutureMock.Number(3.0)));
         }
 
+        [Test]
+        public void CalculateAsyncInFunction()
+        {
+            Test(@"var f = x => {
+  var y = await calcAsync(20);
+  return x + y;
+};
+
+f(5)", 10.0, engine => engine.SetFunction("calcAsync", FutureMock.Number(5.0)));
+        }
+
+        [Test]
+        public void CalculateMultipleAsyncInFunction()
+        {
+            Test(@"var f = x => {
+  var y = await barAsync(20);
+  var z = await fooAsync(5);
+  return x + y * z;
+};
+
+f(5)", 80, engine =>
+            {
+                engine.SetFunction("barAsync", FutureMock.Number(5.0));
+                engine.SetFunction("fooAsync", FutureMock.Number(15.0));
+            });
+        }
+
         private static void Test(String sourceCode, Object expected, Action<Engine> setup)
         {
             var engine = new Engine();
